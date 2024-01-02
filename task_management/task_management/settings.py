9f884f0e2bf200483f8e836b384e6f4fcd4fdb42
gagan <20bcs085@iiitdmj.ac.in>
2024-01-02 21:48:03 +0530

    f3

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks.apps.TasksConfig',
+    'tasks',
     'rest_framework',
     'rest_framework.authtoken',
 ]
